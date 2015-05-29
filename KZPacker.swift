cca18242a15f8f033d1cff76b5b3302d66388eb7
katopz <katopz@gmail.com>
2015-05-29 12:29:46 +0700

    Update KZPacker.swift

--- KZPacker.swift
+++ KZPacker.swift
@@ -9,39 +9,7 @@
 import Foundation
 
 class KZPacker {
-    
-    /*
-    let string = "{\"foo\":\"bar\"}"
-    let key = "foos"
-    var old_data = [1:string, 2:string, 3:string]
-    var new_data = [2:string, 4:string]
-    
-    // save
-    KZPacker.pack(key, old_data)
-    
-    // load
-    var data = KZPacker.unpack(key) as! [Int:String]
-    println("get    : \(data)")
-    
-    // sort
-    var sorted_data = sorted(data) { $0.0 > $1.0 }
-    println("sorted : \(sorted_data)")
-    
-    // merge
-    for item in new_data {
-        old_data[item.0] = item.1
-    }
-    println("merged : \(old_data)")
-    
-    // remove
-    println("remove : " + key)
-    KZPacker.remove(key)
-    
-    if let del_data = KZPacker.unpack(key) as? [Int:String] {
-        println(" This should be seen : \(del_data)")
-    }
-    */
-    
+
     private static var _defaults = NSUserDefaults.standardUserDefaults()
     
     static func pack(key:String, _ data:AnyObject) {
