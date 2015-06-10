2d4fea83e9487fc4c0c4115fecfa55349d75fe86
katopz <katopz@gmail.com>
2015-06-11 00:50:56 +0700

    add lazy detect i4s

--- KZSwift.swift
+++ KZSwift.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 func println(object: Any) {
     #if DEBUG
@@ -18,6 +19,12 @@ class KZSwift {
     class var isSimulator: Bool {
         get {
             return TARGET_IPHONE_SIMULATOR != 0
+        }
+    }
+    
+    class var isH480: Bool {
+        get {
+            return UIScreen.mainScreen().bounds.height == 480
         }
     }
 }
