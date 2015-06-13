07aa318ef987051d352a7bebd8a9cd2211473581
katopz <katopz@gmail.com>
2015-06-13 17:27:25 +0700

    add lazy fade in, out

---
+++ KZUIView.swift
@@ -0,0 +1,29 @@
+//
+//  KZUIView.swift
+//  EnvatoMiner
+//
+//  Created by Todsaporn Banjerdkit on 6/13/15.
+//  Copyright (c) 2015 Debokeh. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    
+    func doAlpha(value:Double) -> UIView {
+        self.alpha = CGFloat(value)
+        return self
+    }
+    
+    func fadeIn(duration:Double, delay: Double = 0, completion: ((Bool) -> Void)? = nil) {
+        UIView.animateWithDuration(duration, delay: delay, options:.CurveEaseOut, animations: {
+            self.alpha = 1
+        }, completion: completion)
+    }
+    
+    func fadeOut(duration:Double, delay: Double = 0, completion: ((Bool) -> Void)? = nil) {
+        UIView.animateWithDuration(duration, delay: delay, options:.CurveEaseOut, animations: {
+            self.alpha = 0
+            }, completion: completion)
+    }
+}
