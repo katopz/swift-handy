f548675e64ff8e243107589f687fe9b205295005
katopz <katopz@gmail.com>
2015-07-13 13:09:56 +0700

    promote lazy async UIImage load from url

---
+++ KZUIImageView.swift
@@ -0,0 +1,19 @@
+//
+//  KZUIImageView.swift
+//
+//  Created by Pi R Square on 7/13/2558 BE.
+//  Copyright © 2558 pirsquare. All rights reserved.
+//
+
+import Foundation
+extension UIImageView {
+    public func imageFromUrl(urlString: String) {
+        if let url = NSURL(string: urlString) {
+            let request = NSURLRequest(URL: url)
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {
+                (response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
+                self.image = UIImage(data: data!)
+            }
+        }
+    }
+}
