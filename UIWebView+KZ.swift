a611e5e595398c74c0e01578b88c06377b7af0ea
katopz <katopz@gmail.com>
2015-07-20 17:20:48 +0700

    correct selectedString, add contentOffset

--- UIWebView+KZ.swift
+++ UIWebView+KZ.swift
@@ -12,7 +12,66 @@ extension UIWebView {
     
     public var selectedString:String {
         get {
-            return self.stringByEvaluatingJavaScriptFromString("document.documentElement.style.webkitUserSelect='none'") ?? ""
+            return self.stringByEvaluatingJavaScriptFromString("window.getSelection().toString()") ?? ""
         }
+    }
+    
+    public var contentOffset:CGPoint {
+       get {
+    //print("position:\(_currentWebView?.scrollView.contentOffset.x)")
+    //print("position:\(_currentWebView?.scrollView.contentOffset.y)")
+    
+    /* Fetch the current selection and its pixel location */
+    let selected = self.stringByEvaluatingJavaScriptFromString("window.getSelection().toString();") ?? ""
+    let positionString = self.stringByEvaluatingJavaScriptFromString("(function () {\n"
+    // Fetch the selection
+    + "var sel = window.getSelection();"
+    + "var node = sel.anchorNode;"
+    
+    // Insert a dummy node that we'll use to find the selection position
+    + "var range = sel.getRangeAt(0);"
+    + "var dummyNode = document.createElement(\"span\");"
+    + "range.insertNode(dummyNode);"
+    
+    // Define the functions we'll use to calculate the dummy node's position
+    + "function Point(x, y) {"
+    + "this.x = x;"
+    + "this.y = y;"
+    + "}"
+    
+    + "function getPoint (o) {"
+    + "var oX = 0;"
+    + "var oY = 0;"
+    + "if (o.offsetParent) {"
+    + "do {"
+    + "oX += o.offsetLeft;"
+    + "oY += o.offsetTop;"
+    + "o=o.offsetParent;"
+    + "} while (o)"
+    + "} else if (o.x) {"
+    + "oX += o.x;"
+    + "oY += o.y;"
+    + "}"
+    + "return new Point(oX, oY);"
+    + "}"
+    
+    // Get the dummy node's position and drop the node
+    + "var p = getPoint(dummyNode);"
+    + "dummyNode.parentNode.removeChild(dummyNode);"
+    
+    // Offset for the current window offset.
+    + "p.x -= window.pageXOffset;"
+    + "p.y -= window.pageYOffset;"
+    
+    // TODO - determine the text line height and offset the arrow accordingly?
+    
+    // Return the coordinates as a CGPointFromString() compatible {x, y} string
+    + "return \"{\" + p.x + \", \" + p.y + \"}\";"
+    + "})();")
+    var position = CGPointFromString(positionString!)
+    position = self.convertPoint(position, fromView: self)
+    
+    return position
+    }
     }
 }
