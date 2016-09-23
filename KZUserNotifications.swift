bb8b3783a65be93adfbee41578c0cbbd83cc5f99
katopz <katopz@gmail.com>
2016-09-23 20:30:49 +0700

    Deprecated KZUIDevice, Notification need test.

---
+++ KZUserNotifications.swift
@@ -0,0 +1,34 @@
+//
+//  KZUserNotifications.swift
+//  EnvatoMiner
+//
+//  Created by Todsaporn Banjerdkit on 9/23/16.
+//  Copyright © 2016 Debokeh. All rights reserved.
+//
+
+//  http://stackoverflow.com/questions/37956482/registering-for-push-notifications-in-xcode-8-swift-3-0
+
+import Foundation
+import UIKit
+import UserNotifications
+
+public extension UIDevice {
+    
+    static func checkAllowedPushNotifications(_ handler:@escaping ((Bool!) -> Void)) {
+        if #available(iOS 10.0, *) {
+            // iOS 10+
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                handler(settings.authorizationStatus == .authorized)
+            }
+        } else {
+            // iOS 8, 9
+            if UIApplication.shared.responds(to: #selector(getter: UIApplication.currentUserNotificationSettings)) {
+                let types = UIApplication.shared.currentUserNotificationSettings!.types
+                let isAllowed = (types.intersection(UIUserNotificationType.alert)) != UIUserNotificationType()
+                handler(isAllowed);
+            } else {
+                handler(UIApplication.shared.isRegisteredForRemoteNotifications)
+            }
+        }
+    }
+}
